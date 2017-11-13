a59bcf5a0cecaac81717b70a930c3a6e5aa5e4d0
Sasan <s.faizollahzadeh@saedubai.com>
2017-11-14 01:36:52 +0400

    Finished Demo one

---
+++ VuforiaOnly/Assets/HorseMoveTo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseMoveTo : MonoBehaviour {
+
+    public Transform HorseTarget;
+    public PicupScript pc;
+    public GameObject cube;
+    private void Start()
+    {
+        pc = cube.GetComponent<PicupScript>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+      //  gameObject.transform.position = Vector3.MoveTowards(transform.position, HorseTarget.position, 0.08f);
+    }
+}
---
+++ VuforiaOnly/Assets/mycamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mycamera : MonoBehaviour {
+    Vector3 rotationss = new Vector3(90f, 0, 0);
+    public GameObject myCameraa;
+    private Quaternion desiredRotation;
+    private bool myCond;
+    // Use this for initialization
+    void Awake () {
+        myCameraa = GameObject.FindGameObjectWithTag("MainCamera");
+        Invoke("myroate", 0.4f);
+	}
+
+    // void Update()
+    //{
+    //    if (myCond)
+    //    {
+    //        if(desiredRotation != Quaternion.identity)
+    //        {
+    //            myCameraa.transform.Rotate(Vector3.left, -60f);
+    //        }
+    //    }
+    //}
+
+    void myroate()
+    {
+        myCameraa.transform.Rotate(Vector3.left, -60f);
+        desiredRotation = Quaternion.identity;
+        myCond = true;
+        Debug.Log("ROtated");
+    }
+
+}
