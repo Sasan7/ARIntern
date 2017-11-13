1430d42f427cdcecfcccf9c3d671d0a294dc5729
Moatassem Waly <m.waly@saedubai.com>
2017-11-13 17:17:57 +0400

    Design and Pathfinding Phase

---
+++ VuforiaOnly/Assets/HorseMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject basket = GameObject.Find("Basket");
+		if (basket)
+			GetComponent<UnityEngine.AI.NavMeshAgent>().destination = basket.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
